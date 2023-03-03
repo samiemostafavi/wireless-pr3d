fix features normalization

benchmarks/mixturemodels/validate_pred.py
@@ -252,9 +252,8 @@ def run_validate_pred_processes(exp_args: list):
             # define x from the conditional dataset
             # select the columns
             cond_columns = []
-            for cond_label in cond_dict:
-                if cond_label in model_dict["condition_labels"]:
-                    cond_columns.append(cond_label)
+            for cond_label in model_dict["condition_labels"]:
+                cond_columns.append(cond_label)
 
             # select columns and sample the rows
             rows = cond_df.select(cond_columns).sample(False, (len(y_points_standard)*2)/cond_df.count(), seed=0).limit(len(y_points_standard))
@@ -263,7 +262,9 @@ def run_validate_pred_processes(exp_args: list):
             # define x numpy list
             x_list = []
             for row in x_rows:
-                x_list.append([row[colm] for colm in cond_columns])
+                x_list.append(
+                    [ row[colm] for colm in cond_columns ]
+                )
             x = np.array(x_list)
 
             # define y points and run the inference