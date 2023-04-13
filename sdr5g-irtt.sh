4738f82fe7507d0ea55ea7dbfac255fea9b94a71
Samie Mostafavi <ssmos@kth.se>
2023-04-13 13:38:32 +0200

    update

--- sdr5g-irtt.sh
+++ sdr5g-irtt.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
+irtt client -i 10ms -d 15m -l 75 -o /home/wlab/irtt_data/sdr5g/rtts_5.json --fill=rand 12.1.1.1
+sleep 30
+
+irtt client -i 10ms -d 15m -l 75 -o /home/wlab/irtt_data/sdr5g/rtts_6.json --fill=rand 12.1.1.1
+sleep 30
+
 irtt client -i 10ms -d 15m -l 75 -o /home/wlab/irtt_data/sdr5g/rtts_7.json --fill=rand 12.1.1.1
-sleep 30
-
-irtt client -i 10ms -d 15m -l 75 -o /home/wlab/irtt_data/sdr5g/rtts_8.json --fill=rand 12.1.1.1
-sleep 30
-
-irtt client -i 10ms -d 15m -l 75 -o /home/wlab/irtt_data/sdr5g/rtts_9.json --fill=rand 12.1.1.1
-sleep 30
-
-irtt client -i 10ms -d 15m -l 75 -o /home/wlab/irtt_data/sdr5g/rtts_10.json --fill=rand 12.1.1.1
-sleep 30
-
-irtt client -i 10ms -d 15m -l 75 -o /home/wlab/irtt_data/sdr5g/rtts_11.json --fill=rand 12.1.1.1
