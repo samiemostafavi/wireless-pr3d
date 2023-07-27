a9246a3f5e0457d71f4c3726b45b2a886f2da801
Samie Mostafavi <ssmos@kth.se>
2023-07-27 17:12:38 +0200

    fix server dir group

--- measurements/docker/measure-upload.sh
+++ measurements/docker/measure-upload.sh
@@ -7,7 +7,7 @@ CL_OUT_DIR=/tmp/$EXP_NAME/client/
 NT_OUT_DIR=/tmp/$EXP_NAME/networkinfo/
 for i in $(seq 1 $ITNUM); do
     (
-        (sleep $SLEEP_DUR && irtt client --tripm=$TRIPM -i $INTERVAL -l $LENGTH -m $MULT -d $DUR -o d --outdir=$CL_OUT_DIR --local=:$CL_PORT $SERVER_IP) &
+        (sleep $SLEEP_DUR && irtt client --tripm=$TRIPM -i $INTERVAL -g $EXP_NAME/edge -l $LENGTH -m $MULT -d $DUR -o d --outdir=$CL_OUT_DIR --local=:$CL_PORT $SERVER_IP) &
         (sleep $SLEEP_DUR && python3 /tmp/adv-mobile-info-recorder.py $DUR $NT_SLEEP $NT_OUT_DIR http://10.42.3.1:50500 device=$NT_DEV) &
         wait
         clfile=$(find "$CL_OUT_DIR" -maxdepth 1 -type f -printf '%T@ %p\n' | sort -n | tail -1 | awk '{print $2}')
